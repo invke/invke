91716b70e6dd49ee9b9ee386ca628aafb9fe508c
montgomery anderson <montgomeryanderson@montgomerys-iMac.local>
2017-08-11 10:18:22 +1200

    Added in titles to the various pages and tests.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,20 +1,34 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "invke"
+  end
+
   test "should get index" do
     get home_index_url
     assert_response :success
+
+    # validate content
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
     get home_about_url
     assert_response :success
+
+    # validate content
+    assert_select "title", "about | #{@base_title}"
   end
 
 
   test "should get interests" do
     get home_interests_url
     assert_response :success
+
+    # validate content
+    assert_select "title", "interests | #{@base_title}"
     assert_select "h2", "Drawing"
     assert_select "h2", "Painting"
     assert_select "h2", "Photography"
@@ -24,6 +38,9 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get sports" do
     get home_sports_url
     assert_response :success
+
+    # validate content
+    assert_select "title", "sports | #{@base_title}"
     assert_select "h2", "Surfing"
     assert_select "h2", "Snowboarding"
     assert_select "h2", "Skiing"
@@ -33,6 +50,9 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get programming" do
     get home_programming_url
     assert_response :success
+
+    # validate content
+    assert_select "title", "programming | #{@base_title}"
     assert_select "h2", "Environment"
     assert_select "h2", "Workstation"
   end
