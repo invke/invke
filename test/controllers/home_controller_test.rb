fd08b6f2bf58cce388b0bee99bb0ecdfc33ab260
montgomery anderson <montgomeryanderson@montgomerys-iMac.local>
2017-08-11 08:55:34 +1200

    Added some basic content clauses to skeleton page tests.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -15,11 +15,19 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get interests" do
     get home_interests_url
     assert_response :success
+    assert_select "h2", "Drawing"
+    assert_select "h2", "Painting"
+    assert_select "h2", "Photography"
+    assert_select "h2", "Gym"
   end
 
   test "should get sports" do
     get home_sports_url
     assert_response :success
+    assert_select "h2", "Surfing"
+    assert_select "h2", "Snowboarding"
+    assert_select "h2", "Skiing"
+    assert_select "h2", "Gym"
   end
 
 end
